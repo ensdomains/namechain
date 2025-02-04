0a4830f6a77b14d83e3cdcf0d37bee979255884b
Makoto Inoue <2630+makoto@users.noreply.github.com>
2025-02-04 14:49:29 +0000

    WIP

--- contracts/src/resolver/OwnedResolver.sol
+++ contracts/src/resolver/OwnedResolver.sol
@@ -1,10 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {console} from "forge-std/console.sol";
 
 /**
  * @title OwnedResolver
  * @dev A basic resolver contract with ownership functionality
  */
-contract OwnedResolver is Ownable(msg.sender) {} 
+contract OwnedResolver {
+    mapping(bytes32 => uint64) public recordVersions;
+    mapping(uint64 => mapping(bytes32 => mapping(uint256 => bytes))) versionable_addresses;
+
+    modifier authorised(bytes32 node) {
+        require(isAuthorised(node));
+        _;
+    }
+
+    function msgSender() public view returns (address) {
+        console.log("msg.sender:", msg.sender);
+        console.log("owner:", owner());
+        return msg.sender;
+    }
+
+    function isAuthorised(bytes32) internal view returns (bool) {
+        console.log("isAuthorised:", msg.sender == owner());
+        console.log("msg.sender:", msg.sender);
+        console.log("owner:", owner());
+        return msg.sender == owner();
+    }
+
+    function setAddr(
+        bytes32 node,
+        uint256 coinType,
+        bytes memory a
+    ) public virtual authorised(node) {
+        versionable_addresses[recordVersions[node]][node][coinType] = a;
+    }
+
+    function addr(
+        bytes32 node,
+        uint256 coinType
+    ) public view returns (bytes memory) {
+        return versionable_addresses[recordVersions[node]][node][coinType];
+    }
+
+} 
--- contracts/test/resolver/OwnedResolver.t.sol
+++ contracts/test/resolver/OwnedResolver.t.sol
@@ -6,31 +6,74 @@ import {OwnedResolver} from "../../src/resolver/OwnedResolver.sol";
 import {VerifiableFactory} from "../../lib/verifiable-factory/src/VerifiableFactory.sol";
 import {TransparentVerifiableProxy} from "../../lib/verifiable-factory/src/TransparentVerifiableProxy.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
+import {console} from "forge-std/console.sol";
 contract OwnedResolverTest is Test {
     VerifiableFactory factory;
     uint256 constant SALT = 12345;
     address public owner;
+    OwnedResolver resolver;
+    bytes32 constant TEST_NODE = bytes32(uint256(1));
+    uint256 constant ETH_COIN_TYPE = 60;
 
     function setUp() public {
         owner = makeAddr("owner");
         factory = new VerifiableFactory();
+        
+        address implementation = address(new OwnedResolver());
+        vm.startPrank(owner);
+        address deployed = factory.deployProxy(implementation, SALT, "");
+        vm.stopPrank();
+        
+        resolver = OwnedResolver(deployed);
     }
 
     function testDeploy() public {
-        address implementation = address(new OwnedResolver());
-        bytes memory data = "";
-        vm.startPrank(owner);
-        address deployed = factory.deployProxy(implementation, SALT, data);
-        vm.stopPrank();
-        assertTrue(deployed != address(0));
-        assertTrue(deployed.code.length > 0);
-        
-        TransparentVerifiableProxy proxy = TransparentVerifiableProxy(payable(deployed));
-        // Check salt matches
+        TransparentVerifiableProxy proxy = TransparentVerifiableProxy(payable(address(resolver)));
         assertEq(proxy.salt(), SALT);
-
-        // Check owner is msg.sender
         assertEq(proxy.owner(), owner);
+        assertEq(resolver.owner(), owner);
+    }
+
+    function testSetAndGetAddr() public {
+        bytes memory ethAddress = abi.encodePacked(address(0x123));
+        
+        console.log("Before startPrank - owner:", owner);
+        
+        vm.startPrank(owner);
+        // address msgSender = resolver.msgSender();
+        // console.log("Returned msgSender:", msgSender);
+        resolver.setAddr(TEST_NODE, ETH_COIN_TYPE, ethAddress);
+        vm.stopPrank();
+        // assertEq(msgSender, owner, "msgSender = owner");
+        bytes memory retrievedAddr = resolver.addr(TEST_NODE, ETH_COIN_TYPE);
+        
+        console.log("retrievedAddr:");
+        console.logBytes(retrievedAddr);
+        // assertEq(retrievedAddr, ethAddress);
+    }
+
+    function testCannotSetAddrIfNotOwner() public {
+        bytes memory ethAddress = abi.encodePacked(address(0x123));
+        
+        vm.startPrank(makeAddr("notOwner"));
+        vm.expectRevert();
+        resolver.setAddr(TEST_NODE, ETH_COIN_TYPE, ethAddress);
+        vm.stopPrank();
+    }
+
+    function testVersionableAddresses() public {
+        bytes memory addr1 = abi.encodePacked(address(0x123));
+        bytes memory addr2 = abi.encodePacked(address(0x456));
+        
+        vm.startPrank(owner);
+        // Set initial address
+        resolver.setAddr(TEST_NODE, ETH_COIN_TYPE, addr1);
+        assertEq(resolver.addr(TEST_NODE, ETH_COIN_TYPE), addr1);
+        
+        // Get current version and verify new address is stored under new version
+        uint64 version = resolver.recordVersions(TEST_NODE);
+        resolver.setAddr(TEST_NODE, ETH_COIN_TYPE, addr2);
+        assertEq(resolver.addr(TEST_NODE, ETH_COIN_TYPE), addr2);
+        vm.stopPrank();
     }
 } 
