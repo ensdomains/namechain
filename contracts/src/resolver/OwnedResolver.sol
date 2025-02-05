64ebc53926ad10955cc87003abc259702c163e33
Makoto Inoue <2630+makoto@users.noreply.github.com>
2025-02-05 13:19:05 +0000

    Use AddrResolver from ens-contracts

--- contracts/src/resolver/OwnedResolver.sol
+++ contracts/src/resolver/OwnedResolver.sol
@@ -3,41 +3,19 @@ pragma solidity ^0.8.13;
 
 import {console} from "forge-std/console.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {AddrResolver} from "@ens/contracts/resolvers/profiles/AddrResolver.sol";
 /**
  * @title OwnedResolver
  * @dev A basic resolver contract with ownership functionality
  */
-contract OwnedResolver is OwnableUpgradeable {
-    mapping(bytes32 => uint64) public recordVersions;
-    mapping(uint64 => mapping(bytes32 => mapping(uint256 => bytes))) versionable_addresses;
+contract OwnedResolver is OwnableUpgradeable, AddrResolver {
 
     function initialize(address _owner) public initializer {
         __Ownable_init(_owner); // Initialize Ownable
     }
 
-    function isAuthorised(bytes32) internal view returns (bool) {
+    function isAuthorised(bytes32) internal view override returns (bool) {
         return msg.sender == owner();
-    }
-
-    modifier authorised(bytes32 node) {
-        require(isAuthorised(node));
-        _;
-    }
-
-
-    function setAddr(
-        bytes32 node,
-        uint256 coinType,
-        bytes memory a
-    ) public virtual authorised(node) {
-        versionable_addresses[recordVersions[node]][node][coinType] = a;
-    }
-
-    function addr(
-        bytes32 node,
-        uint256 coinType
-    ) public view returns (bytes memory) {
-        return versionable_addresses[recordVersions[node]][node][coinType];
     }
 
 } 
