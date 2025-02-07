8c8ce29ed488a956f65d8723fc0017a08aef21cd
Ram <ram@hiddentao.com>
2025-02-07 22:45:31 +0800

    refactor: improvement to expiry logic (#12)
    * refactor: improvement to expiry logic
    
    * test: fixed tests
    
    * Update contracts/src/registry/BaseRegistry.sol
    
    Co-authored-by: Nick Johnson <arachnid@notdot.net>
    
    ---------
    
    Co-authored-by: Nick Johnson <arachnid@notdot.net>

--- contracts/test/ETHRegistry.t.sol
+++ contracts/test/ETHRegistry.t.sol
@@ -64,10 +64,12 @@ contract TestETHRegistry is Test, ERC1155Holder {
         vm.assertEq(oldTokenId, newTokenId);
     }
 
-    function testFail_cannot_mint_duplicates() public {
+    function test_Revert_cannot_mint_duplicates() public {
         uint96 flags = registry.FLAG_SUBREGISTRY_LOCKED() | registry.FLAG_RESOLVER_LOCKED();
 
         registry.register("test2", address(this), registry, flags, uint64(block.timestamp) + 86400);
+
+        vm.expectRevert(abi.encodeWithSelector(ETHRegistry.NameAlreadyRegistered.selector, "test2"));
         registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 86400);
     }
 
@@ -77,9 +79,11 @@ contract TestETHRegistry is Test, ERC1155Holder {
         vm.assertEq(address(registry.getSubregistry("test2")), address(this));
     }
 
-    function testFail_cannot_set_locked_subregistry() public {
+    function test_Revert_cannot_set_locked_subregistry() public {
         uint96 flags = registry.FLAG_SUBREGISTRY_LOCKED();
         uint256 tokenId = registry.register("test2", address(this), registry, flags, uint64(block.timestamp) + 86400);
+
+        vm.expectRevert(abi.encodeWithSelector(BaseRegistry.InvalidSubregistryFlags.selector, tokenId, registry.FLAG_SUBREGISTRY_LOCKED(), 0));
         registry.setSubregistry(tokenId, IRegistry(address(this)));
     }
 
@@ -89,16 +93,20 @@ contract TestETHRegistry is Test, ERC1155Holder {
         vm.assertEq(address(registry.getResolver("test2")), address(this));
     }
 
-    function testFail_cannot_set_locked_resolver() public {
+    function test_Revert_cannot_set_locked_resolver() public {
         uint96 flags = registry.FLAG_RESOLVER_LOCKED();
         uint256 tokenId = registry.register("test2", address(this), registry, flags, uint64(block.timestamp) + 86400);
+
+        vm.expectRevert(abi.encodeWithSelector(BaseRegistry.InvalidSubregistryFlags.selector, tokenId, registry.FLAG_RESOLVER_LOCKED(), 0));
         registry.setResolver(tokenId, address(this));
     }
 
-    function testFail_cannot_set_locked_flags() public {
+    function test_Revert_cannot_set_locked_flags() public {
         uint96 flags = registry.FLAG_FLAGS_LOCKED();
         uint256 tokenId = registry.register("test2", address(this), registry, flags, uint64(block.timestamp) + 86400);
-        registry.lock(tokenId, registry.FLAG_RESOLVER_LOCKED());
+
+        vm.expectRevert(abi.encodeWithSelector(BaseRegistry.InvalidSubregistryFlags.selector, tokenId, registry.FLAG_FLAGS_LOCKED(), 0));
+        registry.lock(tokenId, flags);
     }
 
     function test_relinquish() public {
@@ -108,11 +116,67 @@ contract TestETHRegistry is Test, ERC1155Holder {
         vm.assertEq(address(registry.getSubregistry("test2")), address(0));
     }
 
-    function testFail_cannot_relinquish_if_not_owner() public {
+    function test_Revert_cannot_relinquish_if_not_owner() public {
         uint256 tokenId = registry.register("test2", address(1), registry, 0, uint64(block.timestamp) + 86400);
+
         vm.prank(address(2));
+        vm.expectRevert(abi.encodeWithSelector(BaseRegistry.AccessDenied.selector, tokenId, address(1), address(2)));
         registry.relinquish(tokenId);
+
         vm.assertEq(registry.ownerOf(tokenId), address(1));
         vm.assertEq(address(registry.getSubregistry("test2")), address(registry));
+    }
+
+    function test_expired_name_has_no_owner() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        vm.warp(block.timestamp + 101);
+        assertEq(registry.ownerOf(tokenId), address(0));
+    }
+
+    function test_expired_name_can_be_reregistered() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        vm.warp(block.timestamp + 101);
+        
+        uint256 newTokenId = registry.register("test2", address(1), registry, 0, uint64(block.timestamp) + 100);
+        assertEq(newTokenId, tokenId);
+        assertEq(registry.ownerOf(newTokenId), address(1));
+    }
+
+    function test_expired_name_returns_zero_subregistry() public {
+        registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        vm.warp(block.timestamp + 101);
+        assertEq(address(registry.getSubregistry("test2")), address(0));
+    }
+
+    function test_expired_name_returns_zero_resolver() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        registry.setResolver(tokenId, address(1));
+        vm.warp(block.timestamp + 101);
+        assertEq(registry.getResolver("test2"), address(0));
+    }
+
+    function test_renew_extends_expiry() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        uint64 newExpiry = uint64(block.timestamp) + 200;
+        registry.renew(tokenId, newExpiry);
+        
+        (uint64 expiry,) = registry.nameData(tokenId);
+        assertEq(expiry, newExpiry);
+    }
+
+    function test_Revert_renew_expired_name() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 100);
+        vm.warp(block.timestamp + 101);
+        
+        vm.expectRevert(abi.encodeWithSelector(ETHRegistry.NameExpired.selector, tokenId));
+        registry.renew(tokenId, uint64(block.timestamp) + 200);
+    }
+
+    function test_Revert_renew_reduce_expiry() public {
+        uint256 tokenId = registry.register("test2", address(this), registry, 0, uint64(block.timestamp) + 200);
+        uint64 newExpiry = uint64(block.timestamp) + 100;
+        
+        vm.expectRevert(abi.encodeWithSelector(ETHRegistry.CannotReduceExpiration.selector, uint64(block.timestamp) + 200, newExpiry));
+        registry.renew(tokenId, newExpiry);
     }
 }
