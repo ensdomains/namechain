8d429e3be93f6d67b0cc8a66d8701e7c77610a38
Ramesh Nair <ram@hiddentao.com>
2025-02-23 16:08:41 +0800

    feat: more powerful access control base contract

---
+++ contracts/src/registry/EnhancedAccessControl.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (access/AccessControl.sol)
+
+pragma solidity ^0.8.20;
+
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+/**
+ * @dev An enhanced version of OpenZeppelin's AccessControl that allows for:
+ * 
+ * - Context-based roles.
+ * - Root context override (0x0) - role assignments in the root context auto-apply to all contexts.
+ * - Removing all assignments of a given role from a context.
+ */
+abstract contract EnhancedAccessControl is Context, ERC165 {
+    error EnhancedAccessControlUnauthorizedAccount(bytes32 context, bytes32 role, address account);
+    error EnhancedAccessControlBadConfirmation();
+
+    event EnhancedAccessControlRoleAdminChanged(bytes32 role, bytes32 previousAdminRole, bytes32 newAdminRole);
+    event EnhancedAccessControlRoleGranted(bytes32 context, bytes32 role, address account, address sender);
+    event EnhancedAccessControlRoleRevoked(bytes32 context, bytes32 role, address account, address sender);
+
+    /** @dev user role within a context. */
+    struct RoleAssignment {
+        bool hasRole;      // Indicates if the user has the role
+        uint256 version;   // Version tying this assignment to the current role version - see _roleVersion below
+    }
+
+    /** 
+     * @dev admin role that controls a given role. 
+     * Role -> AdminRole
+     */
+    mapping(bytes32 role => bytes32 adminRole) private _adminRoles;
+
+    /**
+     * @dev user role within a context.
+     * Context -> Role -> User -> RoleAssignment
+     */
+    mapping(bytes32 context => mapping(bytes32 role => mapping(address account => RoleAssignment roleAssignment))) private _roles;
+
+    /**
+     * @dev We use a version number to track the changes to the role assignments.
+     * This means we can easily remove all assignments of a given role without needing a loop.
+     */
+    mapping(bytes32 context => mapping(bytes32 role => uint256 version)) private _roleVersion;
+
+    /**
+     * @dev The root context.
+     */
+    bytes32 public constant ROOT_CONTEXT = 0x00;
+
+    /**
+     * @dev The default admin role.
+     */
+    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+
+    /**
+     * @dev Modifier that checks that sender has a specific role within the given context. 
+     * Reverts with an {AccessControlUnauthorizedAccount} error including the required role.
+     */
+    modifier onlyRole(bytes32 context, bytes32 role) {
+        _checkRole(context, role);
+        _;
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(EnhancedAccessControl).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
+    function hasRole(bytes32 context, bytes32 role, address account) public view virtual returns (bool) {
+        uint256 roleVersion = _roleVersion[context][role];
+        uint256 roleVersionInRoot = _roleVersion[ROOT_CONTEXT][role];
+
+        return (_roles[context][role][account].hasRole && _roles[context][role][account].version == roleVersion) ||
+               (_roles[ROOT_CONTEXT][role][account].hasRole && _roles[ROOT_CONTEXT][role][account].version == roleVersionInRoot);
+    }
+
+    /**
+     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `_msgSender()`
+     * is missing `role`. Overriding this function changes the behavior of the {onlyRole} modifier.
+     */
+    function _checkRole(bytes32 context, bytes32 role) internal view virtual {
+        _checkRole(context, role, _msgSender());
+    }
+
+    /**
+     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `account`
+     * is missing `role`.
+     */
+    function _checkRole(bytes32 context, bytes32 role, address account) internal view virtual {
+        if (!hasRole(context, role, account)) {
+            revert EnhancedAccessControlUnauthorizedAccount(context, role, account);
+        }
+    }
+
+    /**
+     * @dev Returns the admin role that controls `role`. See {grantRole} and
+     * {revokeRole}.
+     *
+     * To change a role's admin, use {_setRoleAdmin}.
+     */
+    function getRoleAdmin(bytes32 role) public view virtual returns (bytes32) {
+        return _adminRoles[role];
+    }
+
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleGranted} event.
+     */
+    function grantRole(bytes32 context, bytes32 role, address account) public virtual onlyRole(context, getRoleAdmin(role)) {
+        _grantRole(context, role, account);
+    }
+
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function revokeRole(bytes32 context, bytes32 role, address account) public virtual onlyRole(context, getRoleAdmin(role)) {
+        _revokeRole(context, role, account);
+    }
+
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been revoked `role`, emits a {RoleRevoked}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `callerConfirmation`.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function renounceRole(bytes32 context, bytes32 role, address callerConfirmation) public virtual {
+        if (callerConfirmation != _msgSender()) {
+            revert EnhancedAccessControlBadConfirmation();
+        }
+
+        _revokeRole(context, role, callerConfirmation);
+    }
+
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
+    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual {
+        bytes32 previousAdminRole = getRoleAdmin(role);
+        _adminRoles[role] = adminRole;
+        emit EnhancedAccessControlRoleAdminChanged(role, previousAdminRole, adminRole);
+    }
+
+    /**
+     * @dev Revoke all assignments of a given role in a given context.
+     */
+    function _revokeRoleAssignments(bytes32 context, bytes32 role) internal virtual {
+        _roleVersion[context][role]++;
+    }
+
+    /**
+     * @dev Attempts to grant `role` to `account` and returns a boolean indicating if `role` was granted.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleGranted} event.
+     */
+    function _grantRole(bytes32 context, bytes32 role, address account) internal virtual returns (bool) {
+        if (!hasRole(context, role, account)) {
+            _roles[context][role][account].hasRole = true;
+            _roles[context][role][account].version = _roleVersion[context][role];
+            emit EnhancedAccessControlRoleGranted(context, role, account, _msgSender());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Attempts to revoke `role` to `account` and returns a boolean indicating if `role` was revoked.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function _revokeRole(bytes32 context, bytes32 role, address account) internal virtual returns (bool) {
+        if (hasRole(context, role, account)) {
+            _roles[context][role][account].hasRole = false;
+            emit EnhancedAccessControlRoleRevoked(context, role, account, _msgSender());
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ contracts/test/EnhancedAccessControl.t.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {EnhancedAccessControl} from "../src/registry/EnhancedAccessControl.sol";
+
+contract MockEnhancedAccessControl is EnhancedAccessControl {
+    constructor() {
+        _grantRole(ROOT_CONTEXT, DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setRoleAdmin(bytes32 role, bytes32 adminRole) external {
+        _setRoleAdmin(role, adminRole);
+    }
+
+    function revokeRoleAssignments(bytes32 context, bytes32 role) external {
+        _revokeRoleAssignments(context, role);
+    }
+}
+
+contract EnhancedAccessControlTest is Test {
+    bytes32 public constant ROLE_A = keccak256("ROLE_A");
+    bytes32 public constant ROLE_B = keccak256("ROLE_B");
+    bytes32 public constant CONTEXT_1 = keccak256("CONTEXT_1");
+    bytes32 public constant CONTEXT_2 = keccak256("CONTEXT_2");
+
+    MockEnhancedAccessControl access;
+    address admin;
+    address user1;
+    address user2;
+
+    function setUp() public {
+        admin = address(this);
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+        access = new MockEnhancedAccessControl();
+    }
+
+    function test_initial_admin_role() public view {
+        assertTrue(access.hasRole(CONTEXT_1, access.DEFAULT_ADMIN_ROLE(), admin));
+        assertTrue(access.hasRole(CONTEXT_2, access.DEFAULT_ADMIN_ROLE(), admin));
+    }
+
+    function test_grant_role() public {
+        vm.recordLogs();
+        
+        access.grantRole(CONTEXT_1, ROLE_A, user1);
+        
+        assertTrue(access.hasRole(CONTEXT_1, ROLE_A, user1));
+        assertFalse(access.hasRole(CONTEXT_2, ROLE_A, user1));
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 1);
+        assertEq(entries[0].topics[0], keccak256("EnhancedAccessControlRoleGranted(bytes32,bytes32,address,address)"));
+        (bytes32 context, bytes32 role, address account, address sender) = abi.decode(entries[0].data, (bytes32, bytes32, address, address));
+        assertEq(context, CONTEXT_1);
+        assertEq(role, ROLE_A);
+        assertEq(account, user1);
+        assertEq(sender, address(this));
+    }
+
+    function test_revoke_role() public {
+        access.grantRole(CONTEXT_1, ROLE_A, user1);
+        
+        vm.recordLogs();
+        access.revokeRole(CONTEXT_1, ROLE_A, user1);
+        
+        assertFalse(access.hasRole(CONTEXT_1, ROLE_A, user1));
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 1);
+        assertEq(entries[0].topics[0], keccak256("EnhancedAccessControlRoleRevoked(bytes32,bytes32,address,address)"));
+        (bytes32 context, bytes32 role, address account, address sender) = abi.decode(entries[0].data, (bytes32, bytes32, address, address));
+        assertEq(context, CONTEXT_1);
+        assertEq(role, ROLE_A);
+        assertEq(account, user1);
+        assertEq(sender, address(this));
+    }
+
+    function test_renounce_role() public {
+        vm.startPrank(user1);
+        access.renounceRole(CONTEXT_1, ROLE_A, user1);
+        assertFalse(access.hasRole(CONTEXT_1, ROLE_A, user1));
+        vm.stopPrank();
+    }
+
+    function test_set_role_admin() public {
+        vm.recordLogs();
+        access.setRoleAdmin(ROLE_A, ROLE_B);
+        
+        assertEq(access.getRoleAdmin(ROLE_A), ROLE_B);
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 1);
+        assertEq(entries[0].topics[0], keccak256("EnhancedAccessControlRoleAdminChanged(bytes32,bytes32,bytes32)"));
+        (bytes32 role, bytes32 previousAdmin, bytes32 newAdmin) = abi.decode(entries[0].data, (bytes32, bytes32, bytes32));
+        assertEq(role, ROLE_A);
+        assertEq(previousAdmin, bytes32(0));
+        assertEq(newAdmin, ROLE_B);
+
+        access.grantRole(CONTEXT_1, ROLE_B, user1);
+
+        vm.prank(user1);
+        access.grantRole(CONTEXT_1, ROLE_A, user2);
+        assertTrue(access.hasRole(CONTEXT_1, ROLE_A, user2));
+
+        vm.prank(user1);
+        access.revokeRole(CONTEXT_1, ROLE_A, user2);
+        assertFalse(access.hasRole(CONTEXT_1, ROLE_A, user2));
+    }
+
+    function test_Revert_unauthorized_grant() public {
+        vm.expectRevert(abi.encodeWithSelector(EnhancedAccessControl.EnhancedAccessControlUnauthorizedAccount.selector, CONTEXT_1, access.DEFAULT_ADMIN_ROLE(), user1));
+        vm.prank(user1);
+        access.grantRole(CONTEXT_1, ROLE_A, user2);
+    }
+
+    function test_Revert_unauthorized_revoke() public {
+        access.grantRole(CONTEXT_1, ROLE_A, user2);
+        
+        vm.expectRevert(abi.encodeWithSelector(EnhancedAccessControl.EnhancedAccessControlUnauthorizedAccount.selector, CONTEXT_1, access.DEFAULT_ADMIN_ROLE(), user1));
+        vm.prank(user1);
+        access.revokeRole(CONTEXT_1, ROLE_A, user2);
+    }
+
+    function test_Revert_bad_renounce_confirmation() public {
+        vm.expectRevert(abi.encodeWithSelector(EnhancedAccessControl.EnhancedAccessControlBadConfirmation.selector));
+        vm.prank(user1);
+        access.renounceRole(CONTEXT_1, ROLE_A, user2);
+    }
+
+    function test_role_isolation() public {
+        access.grantRole(CONTEXT_1, ROLE_A, user1);
+        assertTrue(access.hasRole(CONTEXT_1, ROLE_A, user1));
+        assertFalse(access.hasRole(CONTEXT_2, ROLE_A, user1));
+    }
+
+    function test_supports_interface() public view {
+        assertTrue(access.supportsInterface(type(EnhancedAccessControl).interfaceId));
+    }
+
+    function test_revoke_role_assignments() public {
+        access.grantRole(CONTEXT_1, ROLE_A, user1);
+        access.grantRole(CONTEXT_1, ROLE_A, user2);
+        access.grantRole(CONTEXT_1, ROLE_B, user1);
+        
+        access.revokeRoleAssignments(CONTEXT_1, ROLE_A);
+        
+        assertFalse(access.hasRole(CONTEXT_1, ROLE_A, user1));
+        assertFalse(access.hasRole(CONTEXT_1, ROLE_A, user2));
+        assertTrue(access.hasRole(CONTEXT_1, ROLE_B, user1));
+    }
+
+    function test_root_context_role_applies_to_all_contexts() public {
+        access.grantRole(access.ROOT_CONTEXT(), ROLE_A, user1);
+        
+        assertTrue(access.hasRole(CONTEXT_1, ROLE_A, user1));
+        assertTrue(access.hasRole(CONTEXT_2, ROLE_A, user1));
+        assertTrue(access.hasRole(keccak256("ANY_OTHER_CONTEXT"), ROLE_A, user1));
+    }
+} 
