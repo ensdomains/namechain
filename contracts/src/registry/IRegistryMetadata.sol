fb2a264aee9d267a6b887f11e34ffaa56f759fa7
Ram <ram@hiddentao.com>
2025-03-07 11:36:43 +0800

    Update contracts/src/registry/IRegistryMetadata.sol
    Co-authored-by: Nick Johnson <arachnid@notdot.net>

--- contracts/src/registry/IRegistryMetadata.sol
+++ contracts/src/registry/IRegistryMetadata.sol
@@ -6,9 +6,9 @@ pragma solidity >=0.8.13;
  */
 interface IRegistryMetadata {
     /**
-     * @dev Fetches the token URI for a node.
-     * @param tokenId The ID of the node to fetch a URI for.
-     * @return The token URI for the node.
+     * @dev Fetches the token URI for a token ID.
+     * @param tokenId The ID of the token to fetch a URI for.
+     * @return The token URI for the token.
      */
     function tokenUri(uint256 tokenId) external view returns (string calldata);
 }
