d28d34c0ca42f2336c8e8894e19cf9237cced4e4
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2025-04-10 12:56:08 +0200

    implement new upgradable User Registry with PermissionedRegistry inheritence

--- contracts/src/common/RegistryRolesMixin.sol
+++ contracts/src/common/RegistryRolesMixin.sol
@@ -17,4 +17,7 @@ abstract contract RegistryRolesMixin {
 
     uint256 internal constant ROLE_SET_TOKEN_OBSERVER = 1 << 4;
     uint256 internal constant ROLE_SET_TOKEN_OBSERVER_ADMIN = ROLE_SET_TOKEN_OBSERVER << 128;
+
+    uint256 internal constant ROLE_UPGRADE = 1 << 5;
+    uint256 internal constant ROLE_UPGRADE_ADMIN = ROLE_UPGRADE << 128;
 }
