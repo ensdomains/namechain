c1e15f219740d85a5276b3cb7a4e20f0f84c94a8
Andrew Raffensperger <raffy@me.com>
2025-08-03 19:49:12 -0700

    test v1 too, fix DNSTLDResolver

--- contracts/test/dns/DNSTLDResolver.mainnet.test.ts
+++ contracts/test/dns/DNSTLDResolver.mainnet.test.ts
@@ -13,8 +13,13 @@ import {
 
 const KNOWN: KnownProfile[] = [
   {
-    name: "brantly.cash",
-    texts: [{ key: "com.twitter", value: "brantlymillegan" }],
+    name: "taytems.xyz",
+    addresses: [
+      {
+        coinType: COIN_TYPE_ETH,
+        value: "0x8e8Db5CcEF88cca9d624701Db544989C996E3216",
+      },
+    ],
   },
   {
     name: "raffy.xyz",
@@ -61,10 +66,10 @@ if (url) {
       [await dnsTLDResolverV1.read.gatewayURL()],
       ["x-batch-gateway:true"],
     ]);
-    for (const name of new Set(KNOWN.map((x) => getLabelAt(x.name, -1)))) {
+    for (const name of ["dnsname.ens.eth"]) {
       await mainnetV2.setupName({
         name,
-        resolverAddress: dnsTLDResolver.address,
+        resolverAddress: await ensRegistry.read.resolver([namehash(name)]),
       });
     }
     return {
@@ -77,19 +82,45 @@ if (url) {
   }
 
   tests = () => {
-    for (const kp of KNOWN) {
-      it(kp.name, async () => {
-        const F = await chain.networkHelpers.loadFixture(fixture);
-        const bundle = bundleCalls(makeResolutions(kp));
-        const [answer, resolver] =
-          await F.mainnetV2.universalResolver.read.resolve([
-            dnsEncodeName(kp.name),
-            bundle.call,
-          ]);
-        expectVar({ resolver }).toEqualAddress(F.dnsTLDResolver.address);
-        bundle.expect(answer);
-      });
-    }
+    const timeout = 15000;
+    describe("v1", () => {
+      for (const kp of KNOWN) {
+        it(kp.name, { timeout }, async () => {
+          const F = await chain.networkHelpers.loadFixture(fixture);
+          await F.mainnetV2.setupName({
+            name: getLabelAt(kp.name, -1),
+            resolverAddress: F.dnsTLDResolverV1.address,
+          });
+          const bundle = bundleCalls(makeResolutions(kp));
+          const [answer, resolver] =
+            await F.mainnetV2.universalResolver.read.resolve([
+              dnsEncodeName(kp.name),
+              bundle.call,
+            ]);
+          expectVar({ resolver }).toEqualAddress(F.dnsTLDResolverV1.address);
+          bundle.expect(answer);
+        });
+      }
+    });
+    describe("v2", () => {
+      for (const kp of KNOWN) {
+        it(kp.name, { timeout }, async () => {
+          const F = await chain.networkHelpers.loadFixture(fixture);
+          await F.mainnetV2.setupName({
+            name: getLabelAt(kp.name, -1),
+            resolverAddress: F.dnsTLDResolver.address,
+          });
+          const bundle = bundleCalls(makeResolutions(kp));
+          const [answer, resolver] =
+            await F.mainnetV2.universalResolver.read.resolve([
+              dnsEncodeName(kp.name),
+              bundle.call,
+            ]);
+          expectVar({ resolver }).toEqualAddress(F.dnsTLDResolver.address);
+          bundle.expect(answer);
+        });
+      }
+    });
   };
 }
 
