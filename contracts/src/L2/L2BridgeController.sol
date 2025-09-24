20e1b478b25a7473a540b6dd539ba8f88a83f800
tate <tate@ens.domains>
2025-09-24 15:22:45 +1000

    l2 folder

--- contracts/src/L2/L2BridgeController.sol
+++ contracts/src/L2/L2BridgeController.sol
@@ -1,74 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import {TransferData} from "../common/TransferData.sol";
-import {IRegistry} from "../common/IRegistry.sol";
-import {IRegistryDatastore} from "../common/IRegistryDatastore.sol";
-import {NameCoder} from "@ens/contracts/utils/NameCoder.sol";
-import {NameUtils} from "../common/NameUtils.sol";
-import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
-import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import {ITokenObserver} from "../common/ITokenObserver.sol";
-import {IPermissionedRegistry} from "../common/IPermissionedRegistry.sol";
-import {EjectionController} from "../common/EjectionController.sol";
-import {IBridge, LibBridgeRoles} from "../common/IBridge.sol";
-import {BridgeEncoder} from "../common/BridgeEncoder.sol";
-import {LibRegistryRoles} from "../common/LibRegistryRoles.sol";
 
-/**
- * @title L2BridgeController
- * @dev Combined controller that handles both ejection messages from L1 to L2 and ejection operations
- */
+import {BridgeEncoder} from "./../common/BridgeEncoder.sol";
+import {EjectionController} from "./../common/EjectionController.sol";
+import {IBridge, LibBridgeRoles} from "./../common/IBridge.sol";
+import {IPermissionedRegistry} from "./../common/IPermissionedRegistry.sol";
+import {IRegistry} from "./../common/IRegistry.sol";
+import {IRegistryDatastore} from "./../common/IRegistryDatastore.sol";
+import {ITokenObserver} from "./../common/ITokenObserver.sol";
+import {LibRegistryRoles} from "./../common/LibRegistryRoles.sol";
+import {NameUtils} from "./../common/NameUtils.sol";
+import {TransferData} from "./../common/TransferData.sol";
+
+/// @title L2BridgeController
+///
+/// @dev Combined controller that handles both ejection messages from L1 to L2 and ejection operations
 contract L2BridgeController is EjectionController, ITokenObserver {
+    ////////////////////////////////////////////////////////////////////////
+    // Constants
+    ////////////////////////////////////////////////////////////////////////
+
+    IRegistryDatastore public immutable DATASTORE;
+
+    ////////////////////////////////////////////////////////////////////////
+    // Errors
+    ////////////////////////////////////////////////////////////////////////
+
     error NotTokenOwner(uint256 tokenId);
+
     error TooManyRoleAssignees(uint256 tokenId, uint256 roleBitmap);
 
-    IRegistryDatastore public immutable datastore;
+    ////////////////////////////////////////////////////////////////////////
+    // Initialization
+    ////////////////////////////////////////////////////////////////////////
 
     constructor(
-        IBridge _bridge,
-        IPermissionedRegistry _registry, 
-        IRegistryDatastore _datastore
-    ) EjectionController(_registry, _bridge) {
-        datastore = _datastore;
-    }   
+        IBridge bridge_,
+        IPermissionedRegistry registry_,
+        IRegistryDatastore datastore_
+    ) EjectionController(registry_, bridge_) {
+        DATASTORE = datastore_;
+    }
 
-    /**
-     * @dev Should be called when a name is being ejected back to L2.
-     *
-     * @param transferData The transfer data for the name being migrated
-     */
+    /// @inheritdoc IERC165
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(EjectionController) returns (bool) {
+        return
+            interfaceId == type(ITokenObserver).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Implementation
+    ////////////////////////////////////////////////////////////////////////
+
+    /// @notice Should be called when a name is being ejected back to L2.
+    ///
+    /// @param transferData The transfer data for the name being migrated
     function completeEjectionFromL1(
         TransferData memory transferData
-    ) 
-    external 
-    virtual 
-    onlyRootRoles(LibBridgeRoles.ROLE_EJECTOR)
-    {
-        (uint256 tokenId,,) = registry.getNameData(transferData.label);
+    ) external virtual onlyRootRoles(LibBridgeRoles.ROLE_EJECTOR) {
+        (uint256 tokenId, , ) = REGISTRY.getNameData(transferData.label);
 
         // owner should be the bridge controller
-        if (registry.ownerOf(tokenId) != address(this)) {
+        if (REGISTRY.ownerOf(tokenId) != address(this)) {
             revert NotTokenOwner(tokenId);
         }
 
-        registry.setSubregistry(tokenId, IRegistry(transferData.subregistry));
-        registry.setResolver(tokenId, transferData.resolver);
+        REGISTRY.setSubregistry(tokenId, IRegistry(transferData.subregistry));
+        REGISTRY.setResolver(tokenId, transferData.resolver);
 
         // now unset the token observer and transfer the name to the owner
-        registry.setTokenObserver(tokenId, ITokenObserver(address(0)));
-        registry.safeTransferFrom(address(this), transferData.owner, tokenId, 1, "");
+        REGISTRY.setTokenObserver(tokenId, ITokenObserver(address(0)));
+        REGISTRY.safeTransferFrom(address(this), transferData.owner, tokenId, 1, "");
 
         bytes memory dnsEncodedName = NameUtils.dnsEncodeEthLabel(transferData.label);
         emit NameEjectedToL2(dnsEncodedName, tokenId);
     }
 
-    /**
-     * @dev Override onERC1155Received to handle minting scenarios
-     * When from is address(0), it's a mint operation and we should just return success
-     * Otherwise, delegate to the parent implementation for ejection processing
-     */
+    // TODO: batch receive?
+
+    /// @notice Override onERC1155Received to handle minting scenarios
+    ///
+    /// @dev When from is address(0), it's a mint operation and we should just return success
+    ///      Otherwise, delegate to the parent implementation for ejection processing
     function onERC1155Received(
         address /* operator */,
         address from,
@@ -80,7 +97,7 @@ contract L2BridgeController is EjectionController, ITokenObserver {
         if (from != address(0)) {
             _processEjection(tokenId, data);
         }
-        
+
         return this.onERC1155Received.selector;
     }
 
@@ -88,14 +105,23 @@ contract L2BridgeController is EjectionController, ITokenObserver {
      * @dev Default implementation of onRenew that does nothing.
      * Can be overridden in derived contracts for custom behavior.
      */
-    function onRenew(uint256 tokenId, uint64 expires, address /*renewedBy*/) external virtual onlyRegistry {
-        bridge.sendMessage(BridgeEncoder.encodeRenewal(tokenId, expires));
+    function onRenew(
+        uint256 tokenId,
+        uint64 expires,
+        address /*renewedBy*/
+    ) external virtual onlyRegistry {
+        BRIDGE.sendMessage(BridgeEncoder.encodeRenewal(tokenId, expires));
     }
 
-    /**
-     * Overrides the EjectionController._onEject function.
-     */
-    function _onEject(uint256[] memory tokenIds, TransferData[] memory transferDataArray) internal virtual override {
+    ////////////////////////////////////////////////////////////////////////
+    // Internal Functions
+    ////////////////////////////////////////////////////////////////////////
+
+    /// @dev Overrides the EjectionController._onEject function.
+    function _onEject(
+        uint256[] memory tokenIds,
+        TransferData[] memory transferDataArray
+    ) internal virtual override {
         for (uint256 i = 0; i < tokenIds.length; i++) {
             uint256 tokenId = tokenIds[i];
             TransferData memory transferData = transferDataArray[i];
@@ -115,31 +141,23 @@ contract L2BridgeController is EjectionController, ITokenObserver {
             We also don't need to check that we (the bridge controller) are the sole assignee of these roles since we exercise these 
             roles further down below.
             */
-            uint256 roleBitmap = 
-                LibRegistryRoles.ROLE_SET_TOKEN_OBSERVER |
+            uint256 roleBitmap = LibRegistryRoles.ROLE_SET_TOKEN_OBSERVER |
                 LibRegistryRoles.ROLE_SET_TOKEN_OBSERVER_ADMIN |
                 LibRegistryRoles.ROLE_SET_SUBREGISTRY |
                 LibRegistryRoles.ROLE_SET_SUBREGISTRY_ADMIN;
-            (uint256 counts, uint256 mask) = registry.getAssigneeCount(tokenId, roleBitmap);
+            (uint256 counts, uint256 mask) = REGISTRY.getAssigneeCount(tokenId, roleBitmap);
             if (counts & mask != roleBitmap) {
                 revert TooManyRoleAssignees(tokenId, roleBitmap);
             }
 
             // NOTE: we don't nullify the resolver here, so that there is no resolution downtime
-            registry.setSubregistry(tokenId, IRegistry(address(0)));
-            registry.setTokenObserver(tokenId, this);
-            
+            REGISTRY.setSubregistry(tokenId, IRegistry(address(0)));
+            REGISTRY.setTokenObserver(tokenId, this);
+
             // Send bridge message for ejection
             bytes memory dnsEncodedName = NameUtils.dnsEncodeEthLabel(transferData.label);
-            bridge.sendMessage(BridgeEncoder.encodeEjection(dnsEncodedName, transferData));
+            BRIDGE.sendMessage(BridgeEncoder.encodeEjection(dnsEncodedName, transferData));
             emit NameEjectedToL1(dnsEncodedName, tokenId);
         }
     }
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(EjectionController) returns (bool) {
-        return interfaceId == type(ITokenObserver).interfaceId || super.supportsInterface(interfaceId);
-    }
-} 
+}
--- contracts/src/L2/UserRegistry.sol
+++ contracts/src/L2/UserRegistry.sol
@@ -1,71 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {SimpleRegistryMetadata} from "../common/SimpleRegistryMetadata.sol";
-import {PermissionedRegistry} from "../common/PermissionedRegistry.sol";
-import {IRegistryDatastore} from "../common/IRegistryDatastore.sol";
-import {IRegistryMetadata} from "../common/IRegistryMetadata.sol";
-import {IRegistry} from "../common/IRegistry.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
+import {IRegistryDatastore} from "./../common/IRegistryDatastore.sol";
+import {IRegistryMetadata} from "./../common/IRegistryMetadata.sol";
+import {PermissionedRegistry} from "./../common/PermissionedRegistry.sol";
+import {SimpleRegistryMetadata} from "./../common/SimpleRegistryMetadata.sol";
 /**
  * @title UserRegistry
  * @dev A user registry that inherits from PermissionedRegistry and is upgradeable using the UUPS pattern.
  * This contract is designed to be deployed via the VerifiableFactory.
  */
 contract UserRegistry is Initializable, PermissionedRegistry, UUPSUpgradeable {
-    uint256 internal constant ROLE_UPGRADE = 1 << 20;
-    uint256 internal constant ROLE_UPGRADE_ADMIN = ROLE_UPGRADE << 128;
+    ////////////////////////////////////////////////////////////////////////
+    // Constants
+    ////////////////////////////////////////////////////////////////////////
 
-    constructor() PermissionedRegistry(IRegistryDatastore(address(0)), IRegistryMetadata(address(0)), _msgSender(), 0) {
+    uint256 internal constant _ROLE_UPGRADE = 1 << 20;
+
+    uint256 internal constant _ROLE_UPGRADE_ADMIN = _ROLE_UPGRADE << 128;
+
+    ////////////////////////////////////////////////////////////////////////
+    // Initialization
+    ////////////////////////////////////////////////////////////////////////
+
+    constructor()
+        PermissionedRegistry(
+            IRegistryDatastore(address(0)),
+            IRegistryMetadata(address(0)),
+            _msgSender(),
+            0
+        )
+    {
         // This disables initialization for the implementation contract
         _disableInitializers();
     }
 
     /**
      * @dev Initializes the UserRegistry contract.
-     * @param _datastore The registry datastore contract.
-     * @param _metadata The registry metadata contract.
-     * @param _deployerRoles The roles to grant to the deployer.
-     * @param _admin The address that will be set as the admin with upgrade privileges.
+     * @param datastore_ The registry datastore contract.
+     * @param metadata_ The registry metadata contract.
+     * @param deployerRoles_ The roles to grant to the deployer.
+     * @param admin_ The address that will be set as the admin with upgrade privileges.
      */
     function initialize(
-        IRegistryDatastore _datastore,
-        IRegistryMetadata _metadata,
-        uint256 _deployerRoles,
-        address _admin
+        IRegistryDatastore datastore_,
+        IRegistryMetadata metadata_,
+        uint256 deployerRoles_,
+        address admin_
     ) public initializer {
-        require(_admin != address(0), "Admin cannot be zero address");
-        
+        // TODO: require => custom error
+        require(admin_ != address(0), "Admin cannot be zero address");
+
         // Initialize datastore
-        datastore = _datastore;
-        
+        datastore = datastore_;
+
         // Initialize metadata provider
-        if (address(_metadata) == address(0)) {
+        if (address(metadata_) == address(0)) {
             // Create a new SimpleRegistryMetadata if none is provided
             _updateMetadataProvider(new SimpleRegistryMetadata());
         } else {
-            metadataProvider = _metadata;
+            metadataProvider = metadata_;
         }
-        
+
         // Grant deployer roles to the admin
-        _grantRoles(ROOT_RESOURCE, _deployerRoles, _admin, false);
+        _grantRoles(ROOT_RESOURCE, deployerRoles_, admin_, false);
     }
 
-    /**
-     * @dev Function that authorizes an upgrade to a new implementation.
-     * Only accounts with the ROLE_UPGRADE_ADMIN role can upgrade the contract.
-     * @param newImplementation The address of the new implementation.
-     */
-    function _authorizeUpgrade(address newImplementation) internal override onlyRootRoles(ROLE_UPGRADE) {
-        // Authorization is handled by the onlyRootRoles modifier
-    }
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
+    /// @inheritdoc IERC165
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(UUPSUpgradeable).interfaceId || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(UUPSUpgradeable).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    /// @inheritdoc UUPSUpgradeable
+    ///
+    /// @dev Only accounts with the ROLE_UPGRADE_ADMIN role can upgrade the contract.
+    ///
+    /// @param newImplementation The address of the new implementation.
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyRootRoles(_ROLE_UPGRADE) {
+        // Authorization is handled by the onlyRootRoles modifier
     }
 }
