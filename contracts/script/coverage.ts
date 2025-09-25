5eb0574dc5bf18b868f99ed4e87ba4de507b8194
Andrew Raffensperger <raffy@me.com>
2025-09-25 13:02:05 -0400

    remove sanitize-lcov and coveralls, replace with genhtml reports and github pages

---
+++ contracts/script/coverage.ts
@@ -0,0 +1,47 @@
+import { execSync } from "node:child_process";
+import { readdirSync, mkdirSync, rmSync } from "node:fs";
+
+const EXT = ".info";
+const cwd = new URL("../coverage/", import.meta.url);
+
+// find coverage files
+const found = readdirSync(cwd).filter((x) => x.endsWith(EXT));
+if (!found.length) {
+  throw new Error("no coverage files, execute: bun run coverage");
+}
+console.table(found);
+
+// clean filtered directory
+const filteredDir = new URL(N(""), cwd);
+rmSync(filteredDir, { recursive: true, force: true });
+mkdirSync(filteredDir, { recursive: true });
+
+// generate individual reports
+for (const name of found) {
+  execSync(
+    `lcov --ignore-errors unused,unused --remove ${name} "lib/*" "*test*" "*mock*" --output-file ${N(name)}`,
+    { cwd },
+  );
+  createReport(name);
+}
+
+// generate combined report
+const name = `all${EXT}`;
+execSync(
+  `lcov --ignore-errors inconsistent,unused --rc branch_coverage=1 --add-tracefile "${N("*")}" --output-file ${N(name)}`,
+  { cwd },
+);
+createReport(name);
+
+function N(name: string) {
+  return `filtered/${name}`;
+}
+
+function createReport(name: string) {
+  const title = name.replace(EXT, "");
+  execSync(
+    `genhtml ${N(name)} --flat --source-directory ../ --output-directory reports/${title}/`,
+    { cwd },
+  );
+  console.log(`Wrote: ${title}`);
+}
