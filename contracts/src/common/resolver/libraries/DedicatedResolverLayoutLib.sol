68c1932e8205d2bb71145fb28e571905d25c4605
Ram <ram@hiddentao.com>
2025-11-03 15:15:20 +0800

    BET-493: Pre-migration script (#146)

--- contracts/src/common/resolver/libraries/DedicatedResolverLayoutLib.sol
+++ contracts/src/common/resolver/libraries/DedicatedResolverLayoutLib.sol
@@ -3,17 +3,17 @@ pragma solidity >=0.8.13;
 
 /// @notice Storage layout of DedicatedResolver.
 library DedicatedResolverLayoutLib {
-    uint256 constant SLOT_ADDRESSES = 0; // _addresses
+    uint256 internal constant SLOT_ADDRESSES = 0; // _addresses
 
-    uint256 constant SLOT_TEXTS = 1; // _texts
+    uint256 internal constant SLOT_TEXTS = 1; // _texts
 
-    uint256 constant SLOT_CONTENTHASH = 2; // _contenthash
+    uint256 internal constant SLOT_CONTENTHASH = 2; // _contenthash
 
-    uint256 constant SLOT_PUBKEY = 3; // _pubkeyX and _pubkeyY
+    uint256 internal constant SLOT_PUBKEY = 3; // _pubkeyX and _pubkeyY
 
-    uint256 constant SLOT_ABIS = 5; // _abis
+    uint256 internal constant SLOT_ABIS = 5; // _abis
 
-    uint256 constant SLOT_INTERFACES = 6; // _interfaces
+    uint256 internal constant SLOT_INTERFACES = 6; // _interfaces
 
-    uint256 constant SLOT_PRIMARY = 7; // _primary
+    uint256 internal constant SLOT_PRIMARY = 7; // _primary
 }
