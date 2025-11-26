71f7c040be57caab490760f205c17d17f5229c1a
Andrew Raffensperger <raffy@me.com>
2025-11-26 12:40:40 -0800

    add deploy script

---
+++ contracts/deploy/l2/02_RegistryFinder.ts
@@ -0,0 +1,20 @@
+import { artifacts, execute } from "@rocketh";
+import { ROLES } from "../constants.js";
+import { dnsEncodeName } from "../../test/utils/utils.ts";
+
+export default execute(
+  async ({ deploy, get, namedAccounts: { deployer } }) => {
+    const ethRegistry =
+      get<(typeof artifacts.PermissionedRegistry)["abi"]>("ETHRegistry");
+
+    await deploy("RegistryFinder", {
+      account: deployer,
+      artifact: artifacts.RegistryFinder,
+      args: [ethRegistry.address, dnsEncodeName("eth")],
+    });
+  },
+  {
+    tags: ["RegistryFinder", "l2"],
+    dependencies: ["ETHRegistry"],
+  },
+);
